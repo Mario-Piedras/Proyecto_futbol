c70670136caeaf81616e62f2dedb9cff6fd64f32
jonathan <jojhanroca@gmail.com>
2025-08-14 11:25:55 -0500

    Guia completa backend

--- Back/src/db/BD.sql
+++ Back/src/db/BD.sql
@@ -1,7 +1,18 @@
-CREATE DATABASE IF NOT EXISTS Futbol_equipos;
+CREATE DATABASE IF NOT EXISTS futbol_equipos;
 USE Futbol_equipos;
 
-CREATE TABLE IF NOT EXISTS Equipos (
+CREATE TABLE IF NOT EXISTS equipos (
     id_equipo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
+    nombre_equipo VARCHAR(100) NOT NULL
 );
+
+-- Insert = POST
+INSERT INTO equipos (nombre_equipo) VALUES 
+('Alemania'),
+('Holanda'),
+('Suecia'),
+('Francia'),
+('Noruega');
+
+-- Select = GET
+SELECT * FROM equipos;
